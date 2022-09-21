76eb05066af00a0a3b9bfd109d2f2b8475077575
rsrulloda <ronell.rulloda@gmail.com>
2022-09-21 14:24:44 -0700

    Initial commit

---
+++ editor.cpp
@@ -0,0 +1,54 @@
+//
+// Created by ronel on 9/21/2022.
+//
+
+#include <iostream>
+#include <fstream>
+
+#include "editor.h"
+
+using namespace std;
+
+editor::editor() {
+
+}
+
+editor::editor(int test) {
+
+}
+
+void editor::setup() {
+
+}
+
+void editor::addStudent() {
+
+}
+
+void editor::recordAssignmentGrade() {
+
+}
+
+void editor::recordTestGrade() {
+
+}
+
+void editor::recordExamGrade() {
+
+}
+
+void editor::changeGrade() {
+
+}
+
+void editor::finalGrade() {
+
+}
+
+void editor::gradeData() {
+
+}
+
+void editor::quit() {
+
+}
---
+++ editor.h
@@ -0,0 +1,28 @@
+//
+// Created by ronel on 9/21/2022.
+//
+
+#ifndef CS_301_ASSIGNMENT_4_EDITOR_H
+#define CS_301_ASSIGNMENT_4_EDITOR_H
+
+class editor {
+public:
+    editor();
+    editor(int test);
+
+    void setup();
+    void addStudent();
+    void recordAssignmentGrade();
+    void recordTestGrade();
+    void recordExamGrade();
+    void changeGrade();
+    void finalGrade();
+    void gradeData();
+    void quit();
+
+private:
+
+};
+
+
+#endif //CS_301_ASSIGNMENT_4_EDITOR_H
